902238cd76d75712f046fe331ead92d3262dff1d
pabitra3362 <pmohanty1435@gmail.com>
2024-08-07 19:02:12 +0530

    till contex

--- frontend/src/component/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/component/ExploreMenu/ExploreMenu.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import {menu_list} from '../../assets/assets'
 
-const ExploreMenu = () => {
+const ExploreMenu = ({category,setCategory}) => {
+  let active="border-4 border-solid border-orange-500 p-1 cursor-pointer min-w-max border-2 border-slate-600 rounded-full"
+  let notActive="cursor-pointer min-w-max border-2 border-slate-600 rounded-full"
   return (
     <div className='explore-menu flex flex-col gap-8'>
       <h1 className='text-5xl font-semibold w-fit m-auto text-center'>Explore our menu</h1>
@@ -9,9 +11,9 @@ const ExploreMenu = () => {
       <div className="explore-menu-list flex justify-between items-center gap-10 text-center mt-5  overflow-x-scroll">
         {menu_list.map((item,index)=>{
             return(
-                <div key={index}>
-                    <img className='cursor-pointer min-w-max' src={item.menu_image} alt=""/>
-                    <p className='text-center font-semibold'>{item.menu_name}</p>
+                <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index}>
+                    <img className={category===item.menu_name?active:notActive} src={item.menu_image} alt=""/>
+                    <p className='text-center font-semibold mt-[10px]'>{item.menu_name}</p>
                 </div>
             )
         })}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../component/Header/Header'
 import ExploreMenu from '../component/ExploreMenu/ExploreMenu'
 
 const Home = () => {
+
+  const [category,setCategory]=useState("All")
+
   return (
    <div>
      <Header/>
-     <ExploreMenu/>
+     <ExploreMenu category={category} setCategory={setCategory} />
    </div>
   )
 }
