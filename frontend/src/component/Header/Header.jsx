2a917b0c61f79b8ddfdceff4e882028b1922c6b6
pabitra3362 <pmohanty1435@gmail.com>
2024-08-10 18:10:15 +0530

    a little responsive

--- frontend/src/component/Header/Header.jsx
+++ frontend/src/component/Header/Header.jsx
@@ -5,11 +5,14 @@ const Header = () => {
   return (
     <div>
       <div className='header' >
-        <div className=' headerjr w-3/4 px-36 pt-36'>
-        <h1 className='text-7xl text-white font-semibold'>Order your favourite food here</h1>
-        <p className='text-white text-lg mt-5'>Choose from a diverse menu featuring a deletctable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
-        <button className='px-10 py-4 bg-white rounded-full mt-5 font-semibold'>View Menu</button>
+        <div className='absolute w-[50%] top-[22%] left-[8%]' >
+        <h1 className='text-[5vw] leading-tight w-[110%]  text-white font-semibold'>Order your favourite food here</h1>
         </div>
+        <div className='absolute w-[70%] top-[58%] left-[8%]'>
+        <p className='text-white text-[1.5vw]'>Choose from a diverse menu featuring a deletctable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+        </div>
+        <div className='absolute bottom-[10%] left-[8%]'><button className='px-10 lg:py-4 lg:w-[150px] lg:text-lg bg-white rounded-full font-semibold md:px-1 md:w-[90px] md:text-xs md:py-2'>View Menu</button></div>
+        
       </div>
     </div>
   )
--- frontend/src/component/navbar/Navbar.jsx
+++ frontend/src/component/navbar/Navbar.jsx
@@ -7,18 +7,18 @@ const Navbar = () => {
     let notActiveExpression='cursor-pointer text-blue-900 font-semibold'
     return (
         <div className='flex p-3 justify-between items-center pt-6'>
-            <div className='tomato-logo'>
-                <img src={assets.logo} alt="" />
+            <div className='tomato-logo md:w-[20%]'>
+                <img className='md:w-[80%]' src={assets.logo} alt="" />
             </div>
             <div className='ul-section'>
-                <ul className='flex gap-8 justify-center items-center'>
+                <ul className='flex lg:gap-8 justify-center items-center md:gap-3'>
                     <li onClick={()=>setActive("Home")} className={active==="Home"?activeExpression:notActiveExpression}>Home</li>
                     <li onClick={()=>setActive("Menu")} className={active==="Menu"?activeExpression:notActiveExpression}>Menu</li>
                     <li onClick={()=>setActive("Mobile-app")} className={active==="Mobile-app"?activeExpression:notActiveExpression}>Mobile app</li>
                     <li onClick={()=>setActive("Contact-us")} className={active==="Contact-us"?activeExpression:notActiveExpression}>Contact us</li>
                 </ul>
             </div>
-            <div className='navbar-right flex justify-center gap-7 items-center'>
+            <div className='navbar-right flex justify-center lg:gap-7 items-center md:gap-4'>
                 <div><img src={assets.search_icon} alt="" /></div>
                 <div className='relative'>
                     <img src={assets.basket_icon} alt="" />
